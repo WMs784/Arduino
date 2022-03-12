c25465918c177bc445998d28ca11e3217b16549a
WMs784 <kosei.t12@gmail.com>
2022-03-12 11:10:59 +0900

    added comment

--- main.cpp
+++ main.cpp
@@ -3,24 +3,27 @@
 
 using namespace std;
 
-int itv = 15;
-int time = millis();
 
 void setup() {
   setup_sensors();
+  Serial.begin(9600);
 }
 void loop() {
-    digitalWrite(GREEN, HIGH);
-    delay(1000);
-    rep(i,180/itv){
-      set_map(dis(),itv*i);
-    }
-    digitalWrite(GREEN, LOW);
-    delay(1000);
-    digitalWrite(BLUE, HIGH);
-    delay(1000);
-    scan();
-    delay(1000);
-    Serial.println(time);
-    cout << time << endl;
+   digitalWrite(GREEN, HIGH);
+   Serial.println("start");
+   delay(1000);
+   rep(i,180/15){
+     set_map(dis(),15*i);
+   }
+   digitalWrite(GREEN, LOW);
+   delay(1000);
+   digitalWrite(BLUE, HIGH);
+   delay(1000);
+   int time = millis();
+   Serial.println(time);
+   scan();
+   Serial.println(time);
+   delay(1000);
+   Serial.println(time);
+   cout << time << endl;
 }
